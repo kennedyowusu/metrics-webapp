aa966e9d7e8c1c0ecc6f61c4d9f833891cd70e6e
kennedyowusu <kennediowusu@gmail.com>
2023-02-08 16:29:55 +0000

    fix linter errors

---
+++ .history/src/redux/slices/countriesSlice_20230208161432.js
@@ -0,0 +1,57 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const API_URL = 'https://restcountries.com/v2/all';
+
+export const fetchCountries = createAsyncThunk(
+  'countries/fetchCountries',
+  async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+  },
+);
+
+const initialState = {
+  countries: [],
+  loading: false,
+  error: null,
+};
+
+const countriesSlice = createSlice({
+  name: 'countries',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchCountries.pending, (state) => {
+      const updateState = { ...state, loading: true, error: null };
+      return updateState;
+    });
+    builder.addCase(fetchCountries.fulfilled, (state, action) => {
+      const updateState = { ...state, loading: false,}
+      state.countries = action.payload;
+      state.loading = false;
+      state.error = null;
+    });
+    builder.addCase(fetchCountries.rejected, (state, action) => {
+      state.countries = [];
+      state.loading = false;
+      state.error = action.error.message;
+    });
+
+    // [fetchCountries.pending]: (state) => {
+    //   state.loading = true;
+    //   state.error = null;
+    // },
+    // [fetchCountries.fulfilled]: (state, action) => {
+    //   state.countries = action.payload;
+    //   state.loading = false;
+    //   state.error = null;
+    // },
+    // [fetchCountries.rejected]: (state, action) => {
+    //   state.countries = [];
+    //   state.loading = false;
+    //   state.error = action.error.message;
+    // },
+  },
+});
+
+export default countriesSlice.reducer;
